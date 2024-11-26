f7db91b156534a2cdee5113a7e39b6a17e0da995
ismlslckk <ismlslck@gmail.com>
2024-11-27 00:12:04 +0100

    refactored

---
+++ libs/ckeditor_full/custom_editor.js
@@ -0,0 +1,522 @@
+
+window.CkEditorBilgi = window.CkEditorBilgi || { disaridanEklenenIcerik: '', guncelIcerik: '' };
+
+class MyUploadAdapter {
+    constructor(loader) {
+        this.loader = loader;
+    }
+    upload() {
+        return this.loader.file.then(
+            (file) => {
+                ckEditorFullFilled = false;
+                // if (file.size / 1024 > 2048) {
+                //     alert('Eklemiş olduğunuz dosya boyutu maksimum sınırı geçmektedir. (Maksimum = 2MB)');
+                //     return new Promise((resolve, reject) => {
+                //         reject();
+                //     });
+                // }
+
+                return new Promise((resolve, reject) => {
+                    const toBase64 = (file) =>
+                        new Promise((resolve, reject) => {
+                            const reader = new FileReader();
+                            reader.readAsDataURL(file);
+                            reader.onload = () => resolve(reader.result);
+                            reader.onerror = (error) => reject(error);
+                        });
+                    const base64_image = toBase64(file).then((data) => {
+                        ckEditorFullFilled = true;
+                        return resolve({
+                            default: data
+                        });
+                    });
+                    this.loader.uploaded = true;
+                    return base64_image;
+                })
+            }
+        );
+    }
+}
+
+let ckEditorFullFilled = true;
+let fileUploadLoader = null;
+let editor;
+const rakamBoyutlari = []
+let selectedImage = null;
+let sifresiCozulecekSmallElement = null;
+
+// This sample still does not showcase all CKEditor 5 features (!)
+// Visit https://ckeditor.com/docs/ckeditor5/latest/features/index.html to browse all the features.
+CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
+    // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html#extended-toolbar-configuration-format
+    toolbar: {
+        items: [
+            /* 'exportPDF', 'exportWord', 'selectAll', '|', */
+            'heading', '|',
+            'bold', 'italic',
+            /*  'strikethrough', 'underline','code', 'subscript', 'superscript', 'removeFormat', */
+            '|',
+            'bulletedList', 'numberedList', 'todoList', '|',
+            /* 'outdent', 'indent', '|', */
+            'undo', 'redo',
+            'fontSize', 'fontFamily', 'fontColor', '|',
+            /*   'fontBackgroundColor','highlight','alignment', '|', */
+            /*  'link',  */
+            'insertImage',
+            /* 'blockQuote', */
+            'insertTable',
+            /* 'mediaEmbed', 'codeBlock', 'htmlEmbed', */
+            '|',
+                    /* 'specialCharacters', 'pageBreak', */
+                    /* 'horizontalLine', '|', */
+                    /* 'textPartLanguage', '|', */
+                    /* 'sourceEditing' */,
+            /* 'findAndReplace' */
+
+        ],
+        shouldNotGroupWhenFull: true
+    },
+    // Changing the language of the interface requires loading the language file using the <script> tag.
+    language: 'tr',
+    list: {
+        properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true
+        }
+    },
+    // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
+    heading: {
+        options: [
+            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+        ]
+    },
+    // https://ckeditor.com/docs/ckeditor5/latest/features/editor-placeholder.html#using-the-editor-configuration
+    placeholder: 'Yazmaya başlayın!!!',
+    // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
+    fontFamily: {
+        options: [
+            'default',
+            'Arial, Helvetica, sans-serif',
+            'Courier New, Courier, monospace',
+            'Georgia, serif',
+            'Lucida Sans Unicode, Lucida Grande, sans-serif',
+            'Tahoma, Geneva, sans-serif',
+            'Times New Roman, Times, serif',
+            'Trebuchet MS, Helvetica, sans-serif',
+            'Verdana, Geneva, sans-serif'
+        ],
+        supportAllValues: true
+    },
+    // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-size-feature
+    fontSize: {
+        options: [10, 12, 14, 'default', 18, 20, 22, 24, 26],
+        supportAllValues: true
+    },
+    // Be careful with the setting below. It instructs CKEditor to accept ALL HTML markup.
+    // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html#enabling-all-html-features
+    htmlSupport: {
+        allow: [
+            {
+                name: /.*/,
+                attributes: true,
+                classes: true,
+                styles: true
+            }
+        ]
+    },
+    // Be careful with enabling previews
+    // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
+    htmlEmbed: {
+        showPreviews: true
+    },
+    // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
+    link: {
+        decorators: {
+            addTargetToExternalLinks: true,
+            defaultProtocol: 'https://',
+            toggleDownloadable: {
+                mode: 'manual',
+                label: 'Downloadable',
+                attributes: {
+                    download: 'file'
+                }
+            }
+        }
+    },
+    // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
+    mention: {
+        feeds: [
+            {
+                marker: '@',
+                feed: [
+                    '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes', '@chocolate', '@cookie', '@cotton', '@cream',
+                    '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread', '@gummi', '@ice', '@jelly-o',
+                    '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding', '@sesame', '@snaps', '@soufflé',
+                    '@sugar', '@sweet', '@topping', '@wafer'
+                ],
+                minimumCharacters: 1
+            }
+        ]
+    },
+    // The "super-build" contains more premium features that require additional configuration, disable them below.
+    // Do not turn them on unless you read the documentation and know how to configure them and setup the editor.
+    removePlugins: [
+        // These two are commercial, but you can try them out without registering to a trial.
+        // 'ExportPdf',
+        // 'ExportWord',
+        'CKBox',
+        'CKFinder',
+        'EasyImage',
+        // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+        // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+        // Storing images as Base64 is usually a very bad idea.
+        // Replace it on production website with other solutions:
+        // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+        'Base64UploadAdapter',
+        'RealTimeCollaborativeComments',
+        'RealTimeCollaborativeTrackChanges',
+        'RealTimeCollaborativeRevisionHistory',
+        'PresenceList',
+        'Comments',
+        'TrackChanges',
+        'TrackChangesData',
+        'RevisionHistory',
+        'Pagination',
+        'WProofreader',
+        // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+        // from a local file system (file://) - load this site via HTTP server if you enable MathType
+        'MathType'
+    ]
+}).then(newEditor => {
+    editor = newEditor;
+
+    editor.model.document.on('change:data', (evt, data) => {
+
+        if ((fileUploadLoader?.loader.status === 'idle' || fileUploadLoader?.loader.status === 'aborted' || !fileUploadLoader)) {
+            const readedData = editor.getData();
+            if (readedData.indexOf('iframe') != -1) {
+                const iframeBasIndexOf = readedData.indexOf('<iframe');
+                const iframeBitIndexOf = readedData.indexOf('</iframe>');
+                if (iframeBasIndexOf != -1 && iframeBitIndexOf != -1) {
+                    const newDataa = readedData.slice(0, iframeBasIndexOf) + readedData.slice(iframeBitIndexOf + 9);
+                    editor.setData(newDataa);
+                }
+            }
+            else if (readedData.indexOf('Version:0.9 StartHTML:') != -1) {
+                const versionIndexOf = readedData.indexOf('Version:0.9 StartHTML:');
+                const blockedIndexOf = readedData.indexOf('SourceURL:about:blank#blocked');
+                const newDataa = readedData.slice(0, versionIndexOf) + readedData.slice(blockedIndexOf + 29);
+                editor.setData(newDataa);
+            }
+            else if (readedData.indexOf('"=""') != -1) {
+                const newDataa = readedData.replaceAll('"=""', '')
+                editor.setData(newDataa);
+            }
+        }
+
+
+    });
+
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        fileUploadLoader = new MyUploadAdapter(loader);
+        return fileUploadLoader;
+    };
+})
+    .catch(error => {
+        console.error('error occured!...');
+        console.error(error);
+    });
+
+setTimeout(() => {
+    turkcelestir('Font Size', 'Yazı Boyutu');
+    turkcelestir('Font Family', 'Yazı Ailesi');
+    turkcelestir('Font Color', 'Yazı Rengi');
+    turkcelestir('Font Background Color', 'Yazı Arkaplan Rengi');
+    turkcelestir('Text alignment', 'Metin Hizalama');
+    turkcelestir('Horizontal line', 'Ayraç Ekle');
+
+    document.querySelectorAll('span').forEach(x => {
+        if (x.innerHTML === 'Find') {
+            x.innerHTML = 'Bul';
+        }
+    });
+
+    ayarlaRakamBoyutlari();
+    ayarlaYaziRengiIcon();
+    okIkonlariniKaldir();
+    secilenResmiAyarla();
+    //secilenMetniSifrelemeButonEkle();
+    uyariAyarla();
+    //ayarlaFindAndReplace();
+}, 100);
+
+function secilenMetniCozmeAyarla() {
+    document.addEventListener('mousedown', function (evt) {
+        if (!!evt && !!evt.target && !!evt.target.tagName) {
+            const selectedTag = evt.target.tagName.toLowerCase();
+            if (selectedTag != 'button') {
+                if (evt.target.className.includes('sifrelenmisEleman')) {
+                    sifresiCozulecekSmallElement = evt.srcElement;
+                }
+                else if (evt.target.parentElement.className.includes('sifrelenmisEleman')) {
+                    sifresiCozulecekSmallElement = evt.target.parentElement;
+                }
+                else {
+                    sifresiCozulecekSmallElement = null;
+                }
+            }
+        }
+    }, false);
+}
+
+function createSmallElementWithAsteriks(pureValue) {
+
+    let asteriksStr = '';
+    for (let i = 0; i < pureValue.length; i++) {
+        asteriksStr = asteriksStr + '✱';
+    }
+
+    let smallElement = document.createElement("span");
+    smallElement.innerText = asteriksStr;;
+    smallElement.className = 'sifrelenmisEleman';
+    smallElement.setAttribute('data-pureValue', pureValue);
+    smallElement = sifreleStyleUygulaVeyaSil(true, smallElement);
+
+    return smallElement;
+}
+
+function sifreleStyleUygulaVeyaSil(uygula = true, smallElement) {
+    if (uygula) {
+        smallElement.style.backgroundColor = '#f4f4f4';
+        smallElement.style.color = '#d63384';
+        smallElement.style.border = '1px solid #ccc';
+        smallElement.style.borderRadius = '3px';
+        smallElement.style.padding = '2px';
+        smallElement.style.fontSize = '13px';
+        return smallElement;
+    }
+    else {
+        smallElement.style.backgroundColor = 'unset';
+        smallElement.style.color = 'unset';
+        smallElement.style.border = 'unset';
+        smallElement.style.borderRadius = 'unset';
+        smallElement.style.padding = 'unset';
+        smallElement.style.fontSize = 'unset';
+        return smallElement;
+    }
+}
+
+function secilenMetniCoz() {
+    sifresiCozulecekSmallElement = sifreleStyleUygulaVeyaSil(false, sifresiCozulecekSmallElement);
+    sifresiCozulecekSmallElement.innerText = sifresiCozulecekSmallElement.getAttribute('data-pureValue');
+    editor.setData(getEditorData());
+}
+
+function secilenMetniSifrele() {
+    const selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        const selectedText = range.toString();
+
+        if (selectedText) {
+            // Create a strong element
+            const resultWithAsteriks = createSmallElementWithAsteriks(selectedText);
+
+            // Replace the selected text with the strong element
+            range.deleteContents();
+            range.insertNode(resultWithAsteriks);
+        }
+    }
+
+    editor.setData(getEditorData());
+}
+
+
+function getEditorData() {
+    return document.getElementsByClassName('ck-content')[0].innerHTML;
+}
+
+function secilenMetniSifrelemeButonEkle() {
+
+    const ckEditorToolbar = document.getElementsByClassName('ck-toolbar__items')[0];
+    const sifreleButonDiv = document.createElement("div");
+    sifreleButonDiv.className = 'ck ck-dropdown';
+
+    // Create a new button element
+    const sifreleButon = document.createElement("button");
+    sifreleButon.className = 'ck ck-button ck-off ck-button';
+    sifreleButon.style.backgroundImage = "url('./png/lock.png')";
+    sifreleButon.style.backgroundSize = "cover"; // Optional: make the image cover the button
+    sifreleButon.style.backgroundPosition = "center";
+    sifreleButon.style.backgroundRepeat = "no-repeat";
+
+
+    const sifreCozButon = document.createElement("button");
+    sifreCozButon.className = 'ck ck-button ck-off ck-button';
+    sifreCozButon.style.backgroundImage = "url('./png/unlock.png')";
+    sifreCozButon.style.backgroundSize = "cover"; // Optional: make the image cover the button
+    sifreCozButon.style.backgroundPosition = "center";
+    sifreCozButon.style.backgroundRepeat = "no-repeat";
+    sifreCozButon.style.marginLeft = '5px';
+    sifreCozButon.style.height = '38px';
+
+    sifreleButon.onclick = secilenMetniSifrele;
+    sifreCozButon.onclick = secilenMetniCoz;
+    sifreleButonDiv.appendChild(sifreleButon);
+    sifreleButonDiv.appendChild(sifreCozButon);
+
+    ckEditorToolbar.appendChild(sifreleButonDiv);
+
+    secilenMetniCozmeAyarla();
+}
+
+function uyariAyarla() {
+
+    const ckEditorToolbar = document.getElementsByClassName('ck-toolbar__items')[0];
+    const uyariDiv = document.createElement("div");
+    uyariDiv.id = 'uyariDiv';
+    uyariDiv.style.position = 'absolute';
+    uyariDiv.style.overflow = 'hidden';
+    uyariDiv.style.right = '0';
+    uyariDiv.style.display = 'none';
+
+    // Create a new button element
+    const p = document.createElement("p");
+    p.id = 'uyariMesajEleman';
+    p.style.color = 'red';
+    p.style.fontWeight = '600';
+    p.style.padding = '5px';
+
+    uyariDiv.appendChild(p);
+
+    ckEditorToolbar.appendChild(uyariDiv);
+
+}
+
+function uyariGoster(text) {
+    const uyariDiv = document.getElementById("uyariDiv");
+    uyariDiv.style.display = 'block';
+
+    const kayanYaziEleman = document.getElementById("uyariMesajEleman");
+    kayanYaziEleman.innerText = text;
+}
+
+function uyariGizle() {
+    const uyariDiv = document.getElementById("uyariDiv");
+    uyariDiv.style.display = 'none';
+}
+
+function okIkonlariniKaldir() {
+    const ikonListesi = document.querySelectorAll('.ck-dropdown__arrow');
+    const ikonListesi2 = document.querySelectorAll('.ck-splitbutton__arrow');
+    ikonListesi.forEach(x => x.remove());
+    ikonListesi2.forEach(x => x.remove());
+}
+
+function turkcelestir(eskiTooltipText, yeniTooltipText) {
+    document.querySelector(`[data-cke-tooltip-text*="${eskiTooltipText}"]`)?.setAttribute('data-cke-tooltip-text', yeniTooltipText);
+}
+
+function guncelIcerigiDoldur() {
+    window.CkEditorBilgi.guncelIcerik = editor.getData();
+}
+
+function editoruDoldur() {
+    editor.setData(window.CkEditorBilgi.disaridanEklenenIcerik);
+}
+
+function rakamSvgAyarla(rakam) {
+
+    const fontSizeIcon = document.querySelector('[data-cke-tooltip-text="Yazı Boyutu"]');
+
+    let img = document.createElement('img');
+    img.src = `./png/${rakam}.png`;
+    img.style.width = '25px';
+    img.style.height = '25px';
+
+    fontSizeIcon.firstElementChild.remove();
+    fontSizeIcon.insertBefore(img, fontSizeIcon.firstChild);
+}
+
+function ayarlaRakamBoyutlari() {
+    rakamSvgAyarla('Default');
+
+    const buttons = document.querySelectorAll(".ck-fontsize-option");
+    buttons.forEach(element => {
+        element.addEventListener("click", (event) => {
+            rakamSvgAyarla(event.srcElement.innerText);
+        });
+    });
+}
+
+function ayarlaYaziRengiIcon() {
+    const yaziRengiIcon = document.querySelector('[data-cke-tooltip-text="Yazı Rengi"]');
+
+    let img = document.createElement('img');
+    img.src = `./png/font_color_icon.png`;
+    img.style.width = '25px';
+    img.style.height = '25px';
+
+    yaziRengiIcon.firstElementChild.remove();
+    yaziRengiIcon.insertBefore(img, yaziRengiIcon.firstChild);
+}
+
+function ayarlaFindAndReplace() {
+    const findAndReplace = document.querySelector('[data-cke-tooltip-text^="Bul"]').parentElement;
+    findAndReplace.firstElementChild.remove();
+
+    let imgFindAndReplace = document.createElement('img');
+    imgFindAndReplace.src = `./png/find_icon.png`;
+    imgFindAndReplace.style.width = '25px';
+    imgFindAndReplace.style.height = '25px';
+
+    imgFindAndReplace.addEventListener('click', () => {
+        const findAndReplaceForm = document.querySelector('.ck-find-and-replace-form').parentElement;
+        if (findAndReplaceForm.style.display === 'block')
+            findAndReplaceForm.style.display = '';
+        else
+            findAndReplaceForm.style.display = 'block';
+    })
+
+    findAndReplace.appendChild(imgFindAndReplace);
+}
+
+function secilenResmiAyarla() {
+    document.addEventListener('mousedown', function (evt) {
+        if (!!evt && !!evt.target && !!evt.target.tagName) {
+            const selectedTag = evt.target.tagName.toLowerCase();
+            if (selectedTag == 'img') {
+                selectedImage = evt.srcElement;
+            }
+        }
+    }, false);
+}
+
+function guncelleSecilenResim(newImgSrc) {
+    if (!!selectedImage) {
+        editor.setData(editor.getData().replaceAll(selectedImage.src, newImgSrc));
+        if (!!selectedImage.srcset) {
+            editor.setData(editor.getData().replaceAll(selectedImage.srcset, newImgSrc));
+        }
+    }
+}
+
+function editoruReadOnlyYap() {
+    document.getElementsByTagName('body')[0].style.pointerEvents = 'none';
+}
+
+function editoruEditableYap() {
+    document.getElementsByTagName('body')[0].style.pointerEvents = '';
+}
+
+window.onerror = function (message, url, lineNumber) {
+    console.log({ message })
+    return true;
+};
